6d7ca8c0287254c3f904f000c8579898651215e9
Mohamed Ahmed Khafagy <mkhafagy.c@KSU.EDU.SA>
2019-08-04 13:35:24 +0300

    adding aws compnents

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AmplifyService } from 'aws-amplify-angular';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AWSFeaturesSample';
+
+  signedIn: boolean;
+  user: any;
+  greeting: string;
+
+  constructor(private amplifyService: AmplifyService) {
+    this.amplifyService.authStateChange$
+      .subscribe(authState => {
+        this.signedIn = authState.state === 'signedIn';
+        if (!authState.user) {
+          this.user = null;
+        } else {
+          this.user = authState.user;
+          this.greeting = 'Hello ' + this.user.username;
+        }
+      });
+  }
+
+  onAlbumImageSelected(event) {
+    window.open(event, '_blank');
+  }
 }
